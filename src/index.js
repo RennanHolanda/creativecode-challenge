47ca48e154710c1e1251cc36bbf5e5ff179a9a61
Bruno Rennan <bruno_rennan@hotmail.com>
2023-04-26 22:00:39 -0300

    Commit inicial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import PhoneBook from "./routes/PhoneBook";
+import NewPhone from "./routes/NewPhone";
+import Home from "./routes/Home";
+import ErrorPage from "./routes/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/newrequest",
+        element: <PhoneBook />,
+      },
+      {
+        path: "/newproduct",
+        element: <NewPhone />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/routes/NewPhone.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPhone = () => {
+  return (
+    <div>NewPhone</div>
+  )
+}
+
+export default NewPhone
